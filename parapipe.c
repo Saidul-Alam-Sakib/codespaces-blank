7b816961310e23bf03a699c44fae12ede0f67b1b
Saidul-Alam-Sakib <u1802010@student.cuet.ac.bd>
2025-08-18 08:57:39 +0000

    Added command parsing function

--- parapipe.c
+++ parapipe.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 #include <pthread.h>
+#include <ctype.h>
+#include <errno.h>
 
 int num_threads;
 char *command;
@@ -74,9 +79,70 @@ char *dequeue(Queue *q) {
 }
 // =====================================================
 
+// ============== Command Parsing ======================
+char **parse_commands(char *cmd_str, int *num_cmds) {
+    // Trim leading spaces
+    while (isspace(*cmd_str)) cmd_str++;
+
+    // Count commands
+    int count = 1;
+    char *p = cmd_str;
+    while ((p = strstr(p, "->"))) {
+        count++;
+        p += 2;
+    }
+
+    char **cmds = malloc(count * sizeof(char *));
+    if (!cmds) {
+        perror("malloc");
+        exit(1);
+    }
+
+    p = cmd_str;
+    char *start = p;
+    int i = 0;
+    while ((p = strstr(p, "->"))) {
+        // Trim trailing spaces before ->
+        char *end = p - 1;
+        while (end > start && isspace(*end)) end--;
+        end++;
+
+        cmds[i] = malloc(end - start + 1);
+        if (!cmds[i]) {
+            perror("malloc");
+            exit(1);
+        }
+        strncpy(cmds[i], start, end - start);
+        cmds[i][end - start] = '\0';
+
+        // Skip -> and trim leading spaces after
+        p += 2;
+        while (isspace(*p)) p++;
+        start = p;
+        i++;
+    }
+
+    // Last command
+    char *end = cmd_str + strlen(cmd_str) - 1;
+    while (end > start && isspace(*end)) end--;
+    end++;
+
+    cmds[i] = malloc(end - start + 1);
+    if (!cmds[i]) {
+        perror("malloc");
+        exit(1);
+    }
+    strncpy(cmds[i], start, end - start);
+    cmds[i][end - start] = '\0';
+
+    *num_cmds = count;
+    return cmds;
+}
+// =====================================================
+
 int main(int argc, char *argv[]) {
     if (argc != 5 || strcmp(argv[1], "-n") != 0 || strcmp(argv[3], "-c") != 0) {
-        fprintf(stderr, "Usage: cat file | ./parapipe -n <num_threads> -c \"cmd\"\n");
+        fprintf(stderr, "Usage: cat file | ./parapipe -n <num_threads> -c \"cmd1 -> cmd2\"\n");
         exit(1);
     }
 
@@ -86,17 +152,20 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    command = argv[4];
-    printf("Threads: %d, Commands: %s\n", num_threads, command);
+    char *cmd_str = argv[4];
 
-    pthread_t threads[num_threads];
-    for (int i = 0; i < num_threads; i++) {
-        pthread_create(&threads[i], NULL, thread_func, NULL);
+    int num_cmds;
+    char **cmds = parse_commands(cmd_str, &num_cmds);
+
+    printf("Threads: %d, Command count: %d\n", num_threads, num_cmds);  // Debug
+    for (int i = 0; i < num_cmds; i++) {
+        printf("Cmd %d: %s\n", i, cmds[i]);
     }
 
-    for (int i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
-    }
+    // Free parsed commands
+    for (int i = 0; i < num_cmds; i++) free(cmds[i]);
+    free(cmds);
 
+    // (Threads and queue setup will be used in later commits)
     return 0;
 }

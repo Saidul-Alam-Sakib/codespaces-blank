babcbf6e0b6cfbd331544b24690da2596b5b1632
Saidul-Alam-Sakib <u1802010@student.cuet.ac.bd>
2025-08-19 17:52:17 +0000

    Implemented worker function: pipeline, processing, and output

--- parapipe.c
+++ parapipe.c
@@ -8,7 +8,7 @@
 #include <ctype.h>
 #include <errno.h>
 
-// ==================== Thread-Safe Queue ====================
+// ================= Thread-Safe Queue =================
 typedef struct Node {
     char *data;
     struct Node *next;
@@ -61,9 +61,9 @@ char *dequeue(Queue *q) {
     free(node);
     return data;
 }
-// ============================================================
+// =====================================================
 
-// ==================== Command Parsing =======================
+// ================ Command Parser =====================
 char **parse_commands(char *cmd_str, int *num_cmds) {
     while (isspace(*cmd_str)) cmd_str++;
 
@@ -117,9 +117,8 @@ char **parse_commands(char *cmd_str, int *num_cmds) {
     *num_cmds = count;
     return cmds;
 }
-// ============================================================
+// =====================================================
 
-// =================== Worker & Receiver Structs ============
 typedef struct {
     Queue *input_q;
     Queue *output_q;
@@ -129,20 +128,115 @@ typedef struct {
 
 typedef struct {
     Queue *q;
-    int n; // number of workers to expect NULLs
+    int n;
 } ReceiverArg;
-// ============================================================
 
-// =================== Thread Skeletons ======================
+// ================ Worker Function ====================
 void *worker_func(void *arg) {
     WorkerArg *wa = (WorkerArg *)arg;
-    // TODO: implement pipeline execution:
-    // 1. dequeue from input_q
-    // 2. run through cmds
-    // 3. enqueue output lines to output_q
-    // 4. enqueue NULL when finished
+    int k = wa->num_cmds;
+    int num_pipes = k + 1;
+    int **pipefds = malloc(num_pipes * sizeof(int *));
+    for (int i = 0; i < num_pipes; i++) {
+        pipefds[i] = malloc(2 * sizeof(int));
+        if (pipe(pipefds[i]) == -1) {
+            perror("pipe");
+            exit(1);
+        }
+    }
+
+    pid_t *pids = malloc(k * sizeof(pid_t));
+    for (int j = 0; j < k; j++) {
+        pids[j] = fork();
+        if (pids[j] == -1) {
+            perror("fork");
+            exit(1);
+        }
+        if (pids[j] == 0) {
+            dup2(pipefds[j][0], STDIN_FILENO);
+            dup2(pipefds[j + 1][1], STDOUT_FILENO);
+            for (int p = 0; p < num_pipes; p++) {
+                close(pipefds[p][0]);
+                close(pipefds[p][1]);
+            }
+            execl("/bin/sh", "sh", "-c", wa->cmds[j], (char *)NULL);
+            perror("execl");
+            exit(1);
+        } else {
+            close(pipefds[j][0]);
+            close(pipefds[j + 1][1]);
+        }
+    }
+
+    int write_fd = pipefds[0][1];
+    int read_fd = pipefds[num_pipes - 1][0];
+    fcntl(read_fd, F_SETFL, O_NONBLOCK);
+
+    while (1) {
+        char *line = dequeue(wa->input_q);
+        if (line == NULL) break;
+        write(write_fd, line, strlen(line));
+        free(line);
+    }
+    close(write_fd);
+
+    char buf[4096];
+    char *out_buf = NULL;
+    size_t out_size = 0;
+    ssize_t r;
+    while (1) {
+        r = read(read_fd, buf, sizeof(buf));
+        if (r == 0) break;
+        if (r == -1) {
+            if (errno == EAGAIN || errno == EWOULDBLOCK) {
+                usleep(1000);
+                continue;
+            } else {
+                perror("read");
+                break;
+            }
+        }
+        out_buf = realloc(out_buf, out_size + r);
+        if (!out_buf) {
+            perror("realloc");
+            exit(1);
+        }
+        memcpy(out_buf + out_size, buf, r);
+        out_size += r;
+
+        char *ptr = out_buf;
+        char *nl;
+        while ((nl = memchr(ptr, '\n', out_size - (ptr - out_buf)))) {
+            *nl = '\0';
+            enqueue(wa->output_q, strdup(ptr));
+            ptr = nl + 1;
+        }
+        size_t leftover = out_size - (ptr - out_buf);
+        memmove(out_buf, ptr, leftover);
+        out_size = leftover;
+    }
+    if (out_size > 0) {
+        out_buf = realloc(out_buf, out_size + 1);
+        out_buf[out_size] = '\0';
+        enqueue(wa->output_q, strdup(out_buf));
+    }
+    free(out_buf);
+    close(read_fd);
+
+    for (int j = 0; j < k; j++) {
+        waitpid(pids[j], NULL, 0);
+    }
+
+    for (int i = 0; i < num_pipes; i++) {
+        free(pipefds[i]);
+    }
+    free(pipefds);
+    free(pids);
+
+    enqueue(wa->output_q, NULL);
     return NULL;
 }
+// =====================================================
 
 void *receiver_func(void *arg) {
     ReceiverArg *ra = (ReceiverArg *)arg;
@@ -155,13 +249,13 @@ void *receiver_func(void *arg) {
             ends++;
             continue;
         }
-        printf("%s", line); // line already contains \n
+        printf("%s\n", line);
         free(line);
     }
     return NULL;
 }
-// ============================================================
 
+// ====================== MAIN =========================
 int main(int argc, char *argv[]) {
     if (argc != 5 || strcmp(argv[1], "-n") != 0 || strcmp(argv[3], "-c") != 0) {
         fprintf(stderr, "Usage: cat file | ./parapipe -n <num_threads> -c \"cmd1 -> cmd2\"\n");
@@ -175,26 +269,29 @@ int main(int argc, char *argv[]) {
     }
 
     char *cmd_str = argv[4];
+
     int num_cmds;
     char **cmds = parse_commands(cmd_str, &num_cmds);
 
-    // Initialize input queues for workers
     Queue *input_queues = malloc(num_threads * sizeof(Queue));
-    if (!input_queues) { perror("malloc"); exit(1); }
-    for (int i = 0; i < num_threads; i++) init_queue(&input_queues[i]);
+    if (!input_queues) {
+        perror("malloc");
+        exit(1);
+    }
+    for (int i = 0; i < num_threads; i++) {
+        init_queue(&input_queues[i]);
+    }
 
-    // Initialize shared output queue
     Queue output_q;
     init_queue(&output_q);
 
-    // Start receiver thread
     ReceiverArg rarg = {&output_q, num_threads};
     pthread_t receiver_tid;
     if (pthread_create(&receiver_tid, NULL, receiver_func, &rarg)) {
-        perror("pthread_create"); exit(1);
+        perror("pthread_create");
+        exit(1);
     }
 
-    // Start worker threads
     pthread_t *worker_tids = malloc(num_threads * sizeof(pthread_t));
     WorkerArg *wargs = malloc(num_threads * sizeof(WorkerArg));
     for (int i = 0; i < num_threads; i++) {
@@ -203,37 +300,36 @@ int main(int argc, char *argv[]) {
         wargs[i].cmds = cmds;
         wargs[i].num_cmds = num_cmds;
         if (pthread_create(&worker_tids[i], NULL, worker_func, &wargs[i])) {
-            perror("pthread_create"); exit(1);
+            perror("pthread_create");
+            exit(1);
         }
     }
 
-    // Read stdin and distribute lines round-robin
     char *line = NULL;
     size_t len = 0;
     ssize_t read_len;
     int thread_idx = 0;
     while ((read_len = getline(&line, &len, stdin)) != -1) {
         char *dup = strdup(line);
-        if (!dup) { perror("strdup"); exit(1); }
+        if (!dup) {
+            perror("strdup");
+            exit(1);
+        }
         enqueue(&input_queues[thread_idx], dup);
         thread_idx = (thread_idx + 1) % num_threads;
     }
     free(line);
 
-    // Signal end to workers
     for (int i = 0; i < num_threads; i++) {
         enqueue(&input_queues[i], NULL);
     }
 
-    // Join worker threads
     for (int i = 0; i < num_threads; i++) {
         pthread_join(worker_tids[i], NULL);
     }
 
-    // Join receiver thread
     pthread_join(receiver_tid, NULL);
 
-    // Cleanup
     for (int i = 0; i < num_cmds; i++) free(cmds[i]);
     free(cmds);
     free(input_queues);
